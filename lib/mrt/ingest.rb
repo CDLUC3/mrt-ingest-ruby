c39b33cbf951f1d7715173a5c30dc3c5e4c4ec4e
Erik Hetzner <erik.hetzner@ucop.edu>
2011-12-19 12:06:26 -0800

    switch to use autoload

--- lib/mrt/ingest.rb
+++ lib/mrt/ingest.rb
@@ -1,15 +1,15 @@
 # Author::    Erik Hetzner  (mailto:erik.hetzner@ucop.edu)
 # Copyright:: Copyright (c) 2011, Regents of the University of California
 
-require 'mrt/ingest/client'
-require 'mrt/ingest/iobject'
-require 'mrt/ingest/message_digest'
-require 'mrt/ingest/one_time_server'
-require 'mrt/ingest/request'
-require 'mrt/ingest/response'
-
 module Mrt
   module Ingest
+    autoload :Client, "mrt/ingest/client"
+    autoload :IObject, "mrt/ingest/iobject"
+    autoload :MessageDigest, "mrt/ingest/message_digest"
+    autoload :OneTimeServer, "mrt/ingest/one_time_server"
+    autoload :Request, "mrt/ingest/request"
+    autoload :Response, "mrt/ingest/response"
+
     class IngestException < Exception
     end
   end
